a00b3bd615fcb5220b27146590daaeaf0ca3b475
Lucky Goswami <lgoswami204@gmail.com>
2024-08-07 00:21:19 +0530

    logic created

--- content-script.js
+++ content-script.js
@@ -1,5 +1,25 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "runScript") {
-    console.log("script started");
+    script();
   }
 });
+
+function script() {
+  const input = document.getElementById("sb_form_q");
+  const searchBtn = document.getElementById("sb_form_go");
+  input.textContent += `a`;
+  searchBtn.click();
+}
+
+// function script() {
+//   chrome.storage.local.set({ numero: "hey dude how are you" }).then(() => {
+//     console.log("value set");
+//   });
+//   for (let i = 0; i < 5; i++) {
+//     console.log(`from script ${i}`);
+//   }
+
+//   chrome.storage.local.get(["numero"]).then((res) => {
+//     console.log(`value is ${res.key}`);
+//   });
+// }
--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
 document.getElementById("go_search").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "runScript" });
-  });
+  for (let i = 0; i < 15; i++) {
+    setTimeout(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "runScript" });
+      });
+      console.log("resquest send");
+    }, i * 7000);
+  }
 });
